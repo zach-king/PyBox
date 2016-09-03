2b1c9adc543e9113b2259e9ac681ba7bc3c76c90
Zachary King <kingzach77@gmail.com>
2016-09-03 00:16:56 -0500

    simple frequency list

---
+++ src/frequency_list.py
@@ -0,0 +1,20 @@
+
+
+class FrequencyList(list):
+    """
+    A subclass of the Python list class; 
+    Keeps track of the frequency of each 
+    item in the list.
+    """
+    def __init__(self, members):
+        super().__init__(members)
+
+    def frequency(self):
+        """Returns a dictionary containing the frequency 
+        of each item, where the item is the key and 
+        the value is the frequency."""
+        counts = {}
+        for item in self:
+            counts.setdefault(item, 0)
+            counts[item] += 1
+        return counts
